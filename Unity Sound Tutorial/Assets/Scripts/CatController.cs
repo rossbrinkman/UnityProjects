0e316e83269f69c8291d9a64b3bb0d67f38b2883
rossbrinkman <55204801+rossbrinkman@users.noreply.github.com>
2019-10-06 22:16:44 -0400

    New sound/animation tutorial

---
+++ Unity Sound Tutorial/Assets/Scripts/CatController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatController : MonoBehaviour
+{
+
+    public AudioSource musicSource;
+
+    public AudioClip musicClip1;
+    public AudioClip musicClip2;
+
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            musicSource.clip = musicClip1;
+            musicSource.Play();
+            anim.SetInteger("State", 1);
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            musicSource.Stop();
+            anim.SetInteger("State", 0);
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            musicSource.clip = musicClip2;
+            musicSource.Play();
+            anim.SetInteger("State", 2);
+        }
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            musicSource.Stop();
+            anim.SetInteger("State", 0);
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            musicSource.loop = true;
+        }
+        if (Input.GetKeyUp(KeyCode.L))
+        {
+            musicSource.loop = false;
+        }
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
