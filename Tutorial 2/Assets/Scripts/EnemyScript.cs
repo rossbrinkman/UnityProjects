fb66cdb08e053cde85af10c4c528dbe888e20998
rossbrinkman <55204801+rossbrinkman@users.noreply.github.com>
2019-10-30 13:40:13 -0400

    Challenge 2 Changes

---
+++ Tutorial 2/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public Vector2 startMarker;
+    public Vector2 endMarker;
+
+    public float speed = 10.0F;
+    private float startTime;
+    private float distance;
+
+    void Start()
+    {
+        // Keep a note of the time the movement started.
+        startTime = Time.time;
+        distance = Vector3.Distance(startMarker, endMarker);
+    }
+
+    // Follows the target position like with a spring
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+        float distanceCovered = (Time.time - startTime) * speed;
+        float howFar = distanceCovered / distance;
+
+        transform.position = Vector3.Lerp(startMarker, endMarker, Mathf.PingPong(howFar, 1));
+    }
+}
--- Tutorial 2/Assets/Scripts/PlayerController.cs
+++ Tutorial 2/Assets/Scripts/PlayerController.cs
@@ -2,22 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody2D rd2d;
+    private Rigidbody2D rb2d;
 
     public float speed;
 
+    public Text livesText;
     public Text score;
+    public Text winText;
 
-    private int scoreValue = 0;
+    private int lives;
+    public int scoreValue = 0;
+    private bool gameOver = false;
+    public bool hasSwitched;
+
+     AudioSource audioSource;
+    public AudioClip audioClip;
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
-        rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString();
+        lives = 3;
+        audioSource = GetComponent<AudioSource>();
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        score.text = "Score: " + scoreValue.ToString();
+        winText.text = "";
+        bool hasSwitched = false;
+        SetLivesText();
     }
 
     // Update is called once per frame
@@ -25,19 +41,46 @@ public class PlayerController : MonoBehaviour
     {
         float hozMovement = Input.GetAxis("Horizontal");
         float vertMovement = Input.GetAxis("Vertical");
-        rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+        rb2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
 
-        
+        if (gameOver == true)
+        {
+            Restart();
+        }
+
     }
 
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.collider.tag == "Coin")
+        if (collision.gameObject.tag == "Coin")
         {
             scoreValue += 1;
-            score.text = scoreValue.ToString();
-            Destroy(collision.collider.gameObject);
+            score.text = "Score: " + scoreValue.ToString();
+            Destroy(collision.gameObject);
+            if (scoreValue == 6 && hasSwitched == false)
+            {
+                StartCoroutine(Wait());
+
+            }
+            if (scoreValue == 12 && hasSwitched == true)
+            {
+                winText.text = "You Win! Game created by Ross Brinkman!\n\nPress R to Restart.";
+                audioSource.clip = audioClip;
+                audioSource.Play();
+                rb2d.velocity = Vector2.zero;
+                rb2d.isKinematic = true;
+                GetComponent<SpriteRenderer>().enabled = false;
+                gameOver = true;
+            }
+        }
+
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(collision.gameObject);
+            collision.gameObject.SetActive(false);
+            lives = lives - 1;
+            SetLivesText();
         }
 
     }
@@ -46,10 +89,47 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.collider.tag == "Ground")
         {
-            if (Input.GetKey(KeyCode.W))
-            {
-                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
-            }
+            Jump();
         }
+    }
+
+
+    void SetLivesText()
+    {
+        livesText.text = "Lives: " + lives.ToString();
+        if (lives <= 0)
+        {
+            winText.text = "You Lose!\n\nPress R to Restart.";
+            rb2d.velocity = Vector2.zero;
+            rb2d.isKinematic = true;
+            GetComponent<SpriteRenderer>().enabled = false;
+            gameOver = true;
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+        }
+    }
+
+    void Restart()
+    {
+        if (Input.GetKey("r"))
+        {
+            SceneManager.LoadScene("Main");
+        }
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(1);
+        transform.position = new Vector3(0, 50f, 0);
+        rb2d.velocity = Vector2.zero;
+        lives = 3;
+        SetLivesText();
+        hasSwitched = true;
     }
 }
